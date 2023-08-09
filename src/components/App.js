3545937109c25241940d432cb4aba562870dc66b
Wandati <Petersimon801@gmail.com>
2023-08-09 18:36:24 +0300

    usestate

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
+import { useState } from "react";
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
